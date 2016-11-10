653dac02990a0dd0a6c4489f6d010c6779c054ec
Arnau Blanch Cortès <arnaubc@gmail.com>
2016-11-10 20:58:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.pr_idi.mydatabaseexample;
+
+
+import java.util.List;
+import java.util.Random;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+public class MainActivity extends ListActivity {
+    private BookData bookData;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        bookData = new BookData(this);
+        bookData.open();
+
+        List<Book> values = bookData.getAllBooks();
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        ArrayAdapter<Book> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+    }
+
+    // Basic method to add pseudo-random list of books so that
+    // you have an example of insertion and deletion
+
+    // Will be called via the onClick attribute
+    // of the buttons in main.xml
+    public void onClick(View view) {
+        @SuppressWarnings("unchecked")
+        ArrayAdapter<Book> adapter = (ArrayAdapter<Book>) getListAdapter();
+        Book book;
+        switch (view.getId()) {
+            case R.id.add:
+                String[] newBook = new String[] { "Miguel Strogoff", "Jules Verne", "Ulysses", "James Joyce", "Don Quijote", "Miguel de Cervantes", "Metamorphosis", "Kafka" };
+                int nextInt = new Random().nextInt(4);
+                // save the new book to the database
+                book = bookData.createBook(newBook[nextInt*2], newBook[nextInt*2 + 1]);
+
+                // After I get the book data, I add it to the adapter
+                adapter.add(book);
+                break;
+            case R.id.delete:
+                if (getListAdapter().getCount() > 0) {
+                    book = (Book) getListAdapter().getItem(0);
+                    bookData.deleteBook(book);
+                    adapter.remove(book);
+                }
+                break;
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    // Life cycle methods. Check whether it is necessary to reimplement them
+
+    @Override
+    protected void onResume() {
+        bookData.open();
+        super.onResume();
+    }
+
+    // Life cycle methods. Check whether it is necessary to reimplement them
+
+    @Override
+    protected void onPause() {
+        bookData.close();
+        super.onPause();
+    }
+
+}

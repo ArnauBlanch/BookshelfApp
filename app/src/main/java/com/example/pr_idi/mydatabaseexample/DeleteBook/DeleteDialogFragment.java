8509ba7b5e454f2fe762b21f114a7d87a3265106
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-01-03 16:13:21 +0100

    Deleted dialog redesign + delete alert & restore snackbar

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/DeleteBook/DeleteDialogFragment.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/DeleteBook/DeleteDialogFragment.java
@@ -3,8 +3,17 @@ package com.example.pr_idi.mydatabaseexample.DeleteBook;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialog;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.TextView;
+
 import com.example.pr_idi.mydatabaseexample.*;
 
 
@@ -12,18 +21,24 @@ public class DeleteDialogFragment extends DialogFragment {
 
     private long bookId;
     private BookData bookData;
+    private Book book;
+    private DeleteDialogListener mListener;
 
+    public interface DeleteDialogListener extends Parcelable {
+        void onBookDeleted(final Book b);
+    }
 
-    public static DeleteDialogFragment newInstance(long bookId) {
+    public static DeleteDialogFragment newInstance(long bookId, DeleteDialogListener listener) {
         DeleteDialogFragment f = new DeleteDialogFragment();
 
         // Supply num input as an argument.
         Bundle args = new Bundle();
         args.putLong("bookId", bookId);
+        args.putParcelable("listener", listener);
         f.setArguments(args);
-
         return f;
     }
+
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -32,12 +47,13 @@ public class DeleteDialogFragment extends DialogFragment {
         bookData.open();
 
         bookId = getArguments().getLong("bookId");
+        mListener = getArguments().getParcelable("listener");
         // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.DeleteDialogTheme);
         builder.setMessage(R.string.dialog_delete_confirmation)
                 .setPositiveButton(R.string.delete, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        deleteBookConfirmed(bookId);
+                        deleteBookConfirmed();
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -45,12 +61,26 @@ public class DeleteDialogFragment extends DialogFragment {
                         // Nothing
                     }
                 });
+        builder.setTitle(R.string.delete_title);
+
+        book = bookData.getBookById(bookId);
+        Log.v("Book:", book.toString());
+        View bookDetailsView = getActivity().getLayoutInflater().inflate(R.layout.delete_book_details, null);
+
+        ((TextView)bookDetailsView.findViewById(R.id.book_title)).setText(book.getTitle());
+        ((TextView)bookDetailsView.findViewById(R.id.book_author)).setText(book.getAuthor());
+        ((TextView)bookDetailsView.findViewById(R.id.book_year)).setText("("+String.valueOf(book.getYear())+")");
+        ((TextView)bookDetailsView.findViewById(R.id.book_publisher)).setText(book.getPublisher());
+        ((TextView)bookDetailsView.findViewById(R.id.book_persEval)).setText(book.getPersonal_evaluation());
+
+        builder.setView(bookDetailsView);
+
         // Create the AlertDialog object and return it
         return builder.create();
     }
 
-    private void deleteBookConfirmed(long bookId) {
-        bookData.deleteBookById(bookId);
-        //listCategoryFragment.updateBooks();
+    private void deleteBookConfirmed() {
+        bookData.deleteBookById(book.getId());
+        mListener.onBookDeleted(book);
     }
 }

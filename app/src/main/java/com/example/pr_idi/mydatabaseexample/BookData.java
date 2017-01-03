c514b605ede70ab2ee73bfa67b466a630e622b61
Arnau Blanch <ArnauBlanch@users.noreply.github.com>
2017-01-03 01:23:41 +0100

    Revert "Merge branch 'part-ivan' with master v1"
    This reverts commit d2829f62e3fa012def34c7f75a0b5a8c5a565b28, reversing
    changes made to 4b1f8c045e29fd95f6e83930a64e5a3742c83d00.

--- app/src/main/java/com/example/pr_idi/mydatabaseexample/BookData.java
+++ app/src/main/java/com/example/pr_idi/mydatabaseexample/BookData.java
@@ -12,6 +12,8 @@ import android.util.Log;
 
 public class BookData {
 
+    private static BookData instance = null;
+
     // Database fields
     private SQLiteDatabase database;
 
@@ -19,11 +21,31 @@ public class BookData {
     private MySQLiteHelper dbHelper;
 
     // Here we only select Title and Author, must select the appropriate columns
-    private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
-            MySQLiteHelper.COLUMN_TITLE, MySQLiteHelper.COLUMN_AUTHOR};
+    private String[] someColumns = {
+            MySQLiteHelper.COLUMN_ID,
+            MySQLiteHelper.COLUMN_TITLE,
+            MySQLiteHelper.COLUMN_AUTHOR
+    };
+
+    private String[] allColumns = {
+            MySQLiteHelper.COLUMN_ID,
+            MySQLiteHelper.COLUMN_TITLE,
+            MySQLiteHelper.COLUMN_AUTHOR,
+            MySQLiteHelper.COLUMN_PUBLISHER,
+            MySQLiteHelper.COLUMN_YEAR,
+            MySQLiteHelper.COLUMN_CATEGORY,
+            MySQLiteHelper.COLUMN_PERSONAL_EVALUATION
+    };
 
     public BookData(Context context) {
         dbHelper = new MySQLiteHelper(context);
+    }
+
+    public static BookData getInstance(Context context) {
+        if (instance == null) {
+            instance = new BookData(context);
+        }
+        return instance;
     }
 
     public void open() throws SQLException {
@@ -36,7 +58,6 @@ public class BookData {
 
     public Book createBook(String title, String author) {
         ContentValues values = new ContentValues();
-        Log.d("Creating", "Creating " + title + " " + author);
 
         // Add data: Note that this method only provides title and author
         // Must modify the method to add the full data
@@ -72,7 +93,6 @@ public class BookData {
         return newBook;
     }
 
-<<<<<<< HEAD
     // Crea nou llibre amb totes les seves dades
     public Book createBook(String title, String author, Integer year, String publisher, String category, String persEval) {
         ContentValues values = new ContentValues();
@@ -98,11 +118,8 @@ public class BookData {
         return newBook;
     }
 
-=======
->>>>>>> part-ivan
     public void deleteBook(Book book) {
         long id = book.getId();
-        System.out.println("Book deleted with id: " + id);
         database.delete(MySQLiteHelper.TABLE_BOOKS, MySQLiteHelper.COLUMN_ID
                 + " = " + id, null);
     }
@@ -129,6 +146,10 @@ public class BookData {
         book.setId(cursor.getLong(0));
         book.setTitle(cursor.getString(1));
         book.setAuthor(cursor.getString(2));
+        book.setPublisher(cursor.getString(3));
+        book.setYear(cursor.getInt(4));
+        book.setCategory(cursor.getString(5));
+        book.setPersonal_evaluation(cursor.getString(6));
         return book;
     }
 }

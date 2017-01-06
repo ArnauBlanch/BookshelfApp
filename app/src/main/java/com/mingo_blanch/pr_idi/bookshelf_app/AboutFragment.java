89421ed7ba65af628427d49e29c3b299582e552c
Arnau Blanch Cortès <arnau.blanch@est.fib.upc.edu>
2017-01-06 18:09:57 +0100

    AboutFragment added

---
+++ app/src/main/java/com/mingo_blanch/pr_idi/bookshelf_app/AboutFragment.java
@@ -0,0 +1,20 @@
+package com.mingo_blanch.pr_idi.bookshelf_app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Arnau on 6/1/17.
+ */
+
+public class AboutFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.about_fragment, container, false);
+    }
+}
